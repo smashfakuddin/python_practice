b205213d9d7b38daacb98ff9a78953a23aa39bf4
smashfakuddin <smashfakuddin.anthro.cou@gmail.com>
2023-06-01 22:27:08 +0600

    first commit

---
+++ condition.py
@@ -0,0 +1,26 @@
+# # Boolean operators
+
+# name = "Ashfak"
+# age = 23
+
+# if name == "Ashfak" and age == 23:
+#     print("Your Name is Ashfak and age are 23 also")
+
+# if name== "john" or name == "Erik":
+#     print("Your Name is John or Erik")
+# else:
+#     print("Not found")
+
+
+
+# # The "in" operator
+
+
+name = input("Enter Your name: ")
+name_list = ["Ashfak","jabed"]
+
+if name in name_list:
+    print("Name Found!")
+else:
+    print("Not Found!")
+
